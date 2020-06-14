c8b899750f0c8e39482e56ff80790920ac7eee91
rob <robcannonxyz@gmail.com>
2020-06-13 18:47:05 -0700

    adding updating outputs to tf12

--- output.tf
+++ output.tf
@@ -1,32 +1,32 @@
 # airflow
 output "webserver_admin_url" {
   description = "Public DNS for the Airflow Webserver instance"
-  value       = "${module.airflow_cluster.webserver_admin_url}"
+  value       = module.airflow_cluster.webserver_admin_url
 }
 
 output "webserver_public_ip" {
   description = "Public IP address for the Airflow Webserver instance"
-  value       = "${module.airflow_cluster.webserver_public_ip}"
+  value       = module.airflow_cluster.webserver_public_ip
 }
 
 output "this_cluster_security_group_id" {
   description = "The ID of the security group"
-  value       = "${module.airflow_cluster.this_cluster_security_group_id}"
+  value       = module.airflow_cluster.this_cluster_security_group_id
 }
 
 output "this_database_security_group_id" {
   description = "The ID of the security group"
-  value       = "${module.airflow_cluster.this_database_security_group_id}"
+  value       = module.airflow_cluster.this_database_security_group_id
 }
 
 output "database_endpoint" {
   description = "Endpoint to connect to RDS metadata DB"
-  value       = "${module.airflow_cluster.database_endpoint}"
+  value       = module.airflow_cluster.database_endpoint
 }
 
 output "database_username" {
   description = "Username to connect to RDS metadata DB"
-  value       = "${module.airflow_cluster.database_username}"
+  value       = module.airflow_cluster.database_username
 }
 
 # emr
