645620c29026aeef155c4049032f87d46e68f769
alicews <44385267+alicews@users.noreply.github.com>
2020-06-15 19:03:29 -0700

    Update run-spark-job.py

--- dags/run-spark-job.py
+++ dags/run-spark-job.py
@@ -1,23 +1,107 @@
+from datetime import timedelta
+
 import airflow
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
-import os
+from airflow.contrib.operators.emr_create_job_flow_operator import EmrCreateJobFlowOperator
+from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
+from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
+from airflow.contrib.operators.emr_terminate_job_flow_operator import EmrTerminateJobFlowOperator
 
-## Define the DAG object
-default_args = {
-    'owner': 'alice',
+DEFAULT_ARGS = {
+    'owner': 'airflow',
     'depends_on_past': False,
     'start_date': airflow.utils.dates.days_ago(0),
-    'retries': 5,
-    'retry_delay': timedelta(minutes=5),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=10),
 }
-dag = DAG(dag_id='run-pyspark-dag', default_args=default_args, schedule_interval="@daily")
 
-# task to submit Spark job
-key = "MY_KEY.pem"
-host =  'user@host.us-west-2.compute.amazonaws.com'
-task1 = BashOperator(
-    task_id='submit-spark',
-    bash_command='ssh -i ' + key + ' ' + host +  'sh  "spark-submit mypyspark.py"',
-    dag=dag)
+dag = DAG(
+    'emr_job_flow_manual_steps_dag',
+    default_args=DEFAULT_ARGS,
+    schedule_interval='@daily'
+)
+
+
+JOB_FLOW_OVERRIDES = {
+    'Name' : 'test-emr',
+    'LogUri' : 's3://my-emr-log-bucket/log.txt',
+    'ReleaseLabel' : 'emr-5.30.0',
+    'Instances' : {
+    'InstanceGroups': [
+            {
+                'Name': 'Master nodes',
+                'Market': 'ON_DEMAND',
+                'InstanceRole': 'MASTER',
+                'InstanceType': 'm5.xlarge',
+                'InstanceCount': 1,
+            },
+            {
+                'Name': 'Slave nodes',
+                'Market': 'ON_DEMAND',
+                'InstanceRole': 'CORE',
+                'InstanceType': 'm5.xlarge',
+                'InstanceCount': 1,
+            }
+        ],
+        'KeepJobFlowAliveWhenNoSteps': True,
+        'TerminationProtected': False
+    },
+    'Applications':[{ 
+        'Name': 'Spark'
+    }],
+    'JobFlowRole':'EMR_EC2_DefaultRole',
+    'ServiceRole':'EMR_DefaultRole'
+}
+
+cluster_creator = EmrCreateJobFlowOperator(
+    task_id='create_job_flow',
+    job_flow_overrides=JOB_FLOW_OVERRIDES,
+    aws_conn_id='aws_default',
+    emr_conn_id='emr_default',
+    dag=dag
+)
+
+
+SPARK_TEST_STEPS = [
+    {
+        'Name': 'test step',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': [
+                'spark-submit', 
+                's3://JOB-PATH/mypyspark.py'. # change the path to your job
+            ]
+        }
+    }
+]
+
+step_adder = EmrAddStepsOperator(
+    task_id='add_steps',
+    job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+    aws_conn_id='aws_default',
+    steps=SPARK_TEST_STEPS,
+    dag=dag
+)
+
+step_checker = EmrStepSensor(
+    task_id='watch_step',
+    job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+    step_id="{{ task_instance.xcom_pull('add_steps', key='return_value')[0] }}",
+    aws_conn_id='aws_default',
+    dag=dag
+)
+
+cluster_remover = EmrTerminateJobFlowOperator(
+    task_id='remove_cluster',
+    job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+    aws_conn_id='aws_default',
+    dag=dag
+)
+
+cluster_creator.set_downstream(step_adder)
+step_adder.set_downstream(step_checker)
+step_checker.set_downstream(cluster_remover)
